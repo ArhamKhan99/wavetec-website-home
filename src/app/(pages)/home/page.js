fe554258f06592bbc170d1ca65ba2453fd0c84b5
ArhamKhan99 <114347698+ArhamKhan99@users.noreply.github.com>
2025-12-01 13:05:18 +0500

    new push

--- src/app/(pages)/home/page.js
+++ src/app/(pages)/home/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <VirtualQueueSection />
 
       {/* <KioskScrollSection prefix ="block2" frameCount = "551"  folder = "/assets/"  frameExt = "png" data={{heading:"Self-Service Kiosks & ATMs",description:"Empower your customers; when, how and where they want.",buttonText:"Discover how it works"}} /> */}
-      <KioskScrollSection prefix ="ezgif-frame" frameCount = "100"  folder = "/assets/"   frameExt = "png" data={{heading:"CX Analytics Platform",description:"Visualize, Measure and take action on every touch point.",buttonText:"Discover how it works"}} value={3}/>
+      {/* <KioskScrollSection prefix ="ezgif-frame" frameCount = "100"  folder = "/assets/"   frameExt = "png" data={{heading:"CX Analytics Platform",description:"Visualize, Measure and take action on every touch point.",buttonText:"Discover how it works"}} value={3}/> */}
       <Integration />
       <KioskScrollSection prefix ="ezgif-frame" frameCount = "100"  folder = "/assets/avatar/"   frameExt = "png" data={{heading:"AI",description:"Take the experience ",buttonText:"Discover how it works"}} value={3}/>
         <GlobalPresence />
